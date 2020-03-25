f603c5e25af281a22177a3ff096bb4addf3757d6
Anthony McKeever <snip3rm00n@gmail.com>
2020-03-24 22:58:28 -0700

    Commit solution for CGI activity

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,14 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist("operand")
+try:
+    total = sum([int(operand) for operand in operands])
+    body = f"Total Sum: {total}"
+except(ValueError, TypeError):
+    body = str("Invalid operand, please use integers only."
+               f"\nSupplied Operands: {operands}")
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
